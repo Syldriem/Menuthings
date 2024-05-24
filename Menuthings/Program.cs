6a5ba840b08afbddad12740db6025dcad14042ad
Syldriem <93327634+Syldriem@users.noreply.github.com>
2024-05-24 10:47:43 +0200

    Add project files.

---
+++ Menuthings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml.Serialization;
+
+namespace Menuthings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Menu");
+            Console.WriteLine("Enter corresponding number to navigate");
+
+            while (true)
+            {
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Pensioner or youth");
+                var choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "0":
+                        Console.WriteLine("You selected option 1");
+                        break;
+                    case "2":
+                        Console.WriteLine("You selected option 2");
+                        break;
+                    case "3":
+                        Console.WriteLine("You selected option 3");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+            }
+        }
+    }
+}
